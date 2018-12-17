6036cf0604c8b54347c8bee2753cedc01a5b6206
Wojtek G <w20g16@gmail.com>
2018-12-17 20:29:34 +0100

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,54 +28,67 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_for_one_user(self):
+    # def test_can_start_a_list_for_one_user(self):
+    #     self.browser.get(self.live_server_url)
+    #
+    #     self.assertIn('To-Do', self.browser.title)
+    #     header_text = self.browser.find_element_by_tag_name('h1').text
+    #     self.assertIn('To-Do', header_text)
+    #
+    #     input_box = self.browser.find_element_by_id('id_new_item')
+    #     self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+    #
+    #     input_box.send_keys('Buy peacock feathers')
+    #     input_box.send_keys(Keys.ENTER)
+    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    #
+    #     input_box = self.browser.find_element_by_id('id_new_item')
+    #     input_box.send_keys('Use peacock feathers to make a fly')
+    #     input_box.send_keys(Keys.ENTER)
+    #
+    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    #     self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+    # def test_multiple_users_can_start_lists_at_different_urls(self):
+    #     self.browser.get(self.live_server_url)
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Buy peacock feathers')
+    #     inputbox.send_keys(Keys.ENTER)
+    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    #
+    #     edith_list_url = self.browser.current_url
+    #     self.assertRegex(edith_list_url, '/lists/.+')
+    #
+    #     self.browser.quit()
+    #     self.browser = webdriver.Firefox()
+    #
+    #     self.browser.get(self.live_server_url)
+    #     page_text = self.browser.find_element_by_tag_name('body').text
+    #     self.assertNotIn('Buy peacock feathers', page_text)
+    #     self.assertNotIn('make a fly', page_text)
+    #
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Buy milk')
+    #     inputbox.send_keys(Keys.ENTER)
+    #     self.wait_for_row_in_list_table('1: Buy milk')
+    #
+    #     francis_list_url = self.browser.current_url
+    #     self.assertRegex(francis_list_url, '/lists/.+')
+    #     self.assertNotEqual(francis_list_url, edith_list_url)
+    #
+    #     page_text = self.browser.find_element_by_tag_name('body').text
+    #     self.assertNotIn('Buy peacock feathers', page_text)
+    #     self.assertIn('Buy milk', page_text)
+
+    def test_layout_and_styling(self):
         self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-
-        input_box = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
-
-        input_box.send_keys('Buy peacock feathers')
-        input_box.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-
-        input_box = self.browser.find_element_by_id('id_new_item')
-        input_box.send_keys('Use peacock feathers to make a fly')
-        input_box.send_keys(Keys.ENTER)
-
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-    def test_multiple_users_can_start_lists_at_different_urls(self):
-        self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
-
-        self.browser.quit()
-        self.browser = webdriver.Firefox()
-
-        self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertNotIn('make a fly', page_text)
-
+        self.wait_for_row_in_list_table('1: testing')
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy milk')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy milk')
-
-        francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/lists/.+')
-        self.assertNotEqual(francis_list_url, edith_list_url)
-
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy milk', page_text)
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2,
+                               512,
+                               delta=10)
